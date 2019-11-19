4ecf688e125f63a6a5fff90bcb47fa92bc4af74d
WalternativE <gregor.beyerle@gmail.com>
2019-11-19 12:10:53 +0100

    Lay out behavior that appears to be odd

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import App from "./App.vue";
 
 Vue.config.productionTip = false;
 
+// this line should fail the build
+this clearly causes an error
+
 new Vue({
   render: h => h(App)
 }).$mount("#app");
